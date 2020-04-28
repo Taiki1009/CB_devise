0d9d86a55cb72d6f000c6593f8dba7903a69023f
taiki011609 <s.taiki011609@gmail.com>
2020-04-28 11:30:40 +0900

    Usersログイン機能

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,18 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  devise_for :users
+  devise_for :users, controllers: {
+    :registrations => 'users/registrations',
+    sessions: 'users/sessions'
+  }  
   
-  
-  root 'home#index'   # deviseでは、signup後にルートに飛ぶ仕様 → 一番最初に来て欲しい画面を設定
+  root 'home#index'   # deviseでは、signup/login後にルートに飛ぶ仕様 → 一番最初に来て欲しい画面を設定
   get 'home/index'
   
+
+  devise_scope :user do
+    get "user/:id", :to => "users/registrations#detail"
+    get "signup", :to => "users/registrations#new"
+    get "login", :to => "users/sessions#new"
+    get "logout", :to => "users/sessions#destroy"
+  end  
 end
